4b8ca34fadef48a2c48afae9a98e45e64f458894
debardf <59962218+debardf@users.noreply.github.com>
2020-11-06 11:35:54 +0100

    création page

--- MVC/index.php
+++ MVC/index.php
@@ -2,8 +2,8 @@
 session_start();
 require_once("util/fonctions.inc.php");
 require_once("util/class.pdoLBC.inc.php");
-include("vues/v_entete.php") ;
-include("vues/v_bandeau.php") ;
+include("vues/v_entete.php");
+include("vues/v_bandeau.php");
 
 if(!isset($_REQUEST['uc']))
      $uc = 'accueil';
