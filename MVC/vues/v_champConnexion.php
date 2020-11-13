a94a67e96e7c50ab1b222f8a120eef6546ab6fd7
maat0 <72969944+maat0@users.noreply.github.com>
2020-11-13 10:03:06 +0100

    Update v_champConnexion.php

--- MVC/vues/v_champConnexion.php
+++ MVC/vues/v_champConnexion.php
@@ -1,4 +1,4 @@
-<form class="champConnexion" action="index.php?uc=frais" method="post">
+<form class="champConnexion" action="index.php?uc=frais&ucf=connexion" method="post">
 <input type="text" name="identifiantConnexion" placeholder="Identifiant">
 <input type="password" name="mdpConnexion" placeholder="Mot de Passe">
 <input type="submit" name="Valider">
