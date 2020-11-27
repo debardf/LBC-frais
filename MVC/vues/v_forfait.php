12c14bd81d4eba2cfaa91fa113f96c0fb7fafd98
viollettes <72963526+viollettes@users.noreply.github.com>
2020-11-27 11:41:28 +0100

    Creer forfait

--- MVC/vues/v_forfait.php
+++ MVC/vues/v_forfait.php
@@ -9,21 +9,27 @@
 	
 	
    <body>
-   <p><h1>Nouveau forfait créé :</h1></p><BR/>
+   <p><h1>Creation d'un nouveau forfait pour la fiche</h1></p><BR/>
 	<form action="c_frais.php?ucf=creerForfait&action=confirmCreatForfait" method="post">
    
 		<table>
 		<tbody>
-			<tr><td>id</td><td><input name="idforfait" size=20></td></tr>
-			<tr><td>libelle </td><td><input name="libelleforfait" size=20></td></tr>	
-			<tr><td>montant</td><td><input name="montant" size=50></td></tr>			
+			<tr><td>id</td><td><input name="id" size=20></td></tr> 
+			<tr><td>matricule </td><td><input type = "hidden" name="matricule" size=20></td></tr>	
+			<tr><td>annee</td><td><input type = "hidden" name="annee" size=50></td></tr>
+			<tr><td>mois</td><td><input type = "hidden" name="mois" size=50></td></tr>
+			<tr><td>quantite</td><td><input name="quantite" size=50></td></tr>
+			<tr><td>valideForfait</td><td><input name="montant" size=50></td></tr>	
 		</tbody>
 		</table>
 		
                 <br/>
 		<input type="submit" value="Valider">
 	</form>
- 
+<php
+//ajouter les libelles des id pour la selection
+?>
 	
 	</body>
 </html>
+
