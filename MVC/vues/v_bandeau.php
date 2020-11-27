8cb8625f1a29440e1b2d3418be164eae5b8f3295
maat0 <72969944+maat0@users.noreply.github.com>
2020-11-27 08:47:35 +0100

    CSS/BANDEAU

--- MVC/vues/v_bandeau.php
+++ MVC/vues/v_bandeau.php
@@ -1,20 +1,21 @@
-<html>
-    <body>
-        <div>
-            <a class="active" href="index.php">Accueil</a>
-            <a href="index.php?uc=frais">Frais</a>
-            <a href="index.php?uc=echantillons">Echantillons</a>
-            <a href="index.php?uc=praticiens">Praticiens</a>
-            <a href="index.php?uc=carrières">Carrières</a>
-            <a href="index.php?uc=visite">Visite</a>
-            <a href="index.php?uc=administration">Administration</a>
-            </br>
-            </br>
-            <a href="index.php?uc=frais&ucf=creerFrais">Créer une note de frais</a>
-            <a href="index.php?uc=frais&ucf=forfait">ajouter un forfait</a>
-            <a href="index.php?uc=frais&ucf=autreForfait">ajouter un autre forfait</a>
-            </br>
-            </br>
-        </div>
-    </body>
-</html>
+<div class="bandeau">
+<!-- Images En-tête -->
+<!--<img src="images/menuGauche.gif"    alt="Choisir" title="Choisir"/>-->
+
+
+<!--  Menu haut-->
+<ul id="menu">
+    <a href="index.php"> Accueil </a>
+    <a href="index.php?uc=frais"> Frais </a>
+    <a href="index.php?uc=echantillons"> Echantillons </a>
+    <a href="index.php?uc=practiciens"> Practiciens </a>
+    <a href="index.php?uc=carrières"> Carrières </a>
+    <a href="index.php?uc=visite"> Visite </a>
+    <a href="index.php?uc=administration"> Administration </a>
+</br>
+</br>
+<li><a href="index.php?uc=frais&ucf=creerForfait"> Créer une note de frais </a></li>
+<li><a href="index.php?uc=frais&ucf=forfait"> Ajouter un frais </a></li>
+<li><a href="index.php?uc=frais&ucf=autreForfait"> Ajouter un autre forfait </a></li>
+</ul>
+</div>
