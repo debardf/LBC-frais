cca9571b131e4edcb817d37358f076d533c4cfcc
viollettes <72963526+viollettes@users.noreply.github.com>
2020-12-09 21:50:06 +0100

    enlever possibilité de suppr/modif forfait et autre forfait quand note valide

--- MVC/vues/v_validerAutreFrais.php
+++ MVC/vues/v_validerAutreFrais.php
@@ -6,6 +6,11 @@
    </br>
 	<form action="index.php?uc=frais&ucf=valider&action=confirmValideAutreFrais&id=<?php echo $id;?>&matricule=<?php echo $matricule?>&annee=<?php echo $annee?>&mois=<?php echo $mois?>"method="post"> 
    		<p text-align="center"><b>Êtes-vous sur de vouloir valider et autre frais ? Une fois validé, cet autre frais ne pourra plus être modifié</b></p>
+            <table>
+            <tr><td>libelle du frais</td><td><input name="libelleAutreForfait" value="<?php echo $libelle?>" size=10 readonly></td></tr>
+            <tr><td>montant</td><td><input name="montant" value="<?php echo $montant ?>" size=10 readonly></td></tr>
+            <tr><td>Date du frais</td><td><input name="dateFrais" value="<?php echo $dateFrais ?>"size=10 readonly></td></tr>
+            </table>
         <br/>
 		<input type="submit" value="Valider">
 	</form>
