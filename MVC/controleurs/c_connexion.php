635d6b9936e95cb3307ddb122c0976f9ca177f84
debardf <59962218+debardf@users.noreply.github.com>
2020-12-30 15:15:54 +0100

    suppression d'un else en trop

--- MVC/controleurs/c_connexion.php
+++ MVC/controleurs/c_connexion.php
@@ -36,14 +36,10 @@ if ($leProfil!=false)
 }
 else
 {
-header('location: index.php?uc=frais?ucf=connexion');
-}
-}
 //Si un des champs est vide on redemande à l'utilisateur de saisir les informations
-else {
 	echo "Le nom d utilisateur et le mot de passe doivent être indiqués ";
 }
-	
+
 
 
 
